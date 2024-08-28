4327dc45237301f8d11e5d7475e641915b5575ac
shsobur <shsoburhossen951@gmail.com>
2024-08-28 16:43:13 +0600

    New Bulider Info section added

--- src/Layout/Pages/HomePageLayout/MainFocus/MainFocus.jsx
+++ src/Layout/Pages/HomePageLayout/MainFocus/MainFocus.jsx
@@ -1,6 +1,12 @@
 import { Link } from "react-router-dom";
 import BuyHomeSvg from "../../../conmponents/AllSvgImages/BuyHomeSvg/BuyHomeSvg";
 import "../MainFocus/MainFocus.css";
+import RentHomeSvg from "../../../conmponents/AllSvgImages/RentHomeSvg/RentHomeSvg";
+import SellHomeSvg from "../../../conmponents/AllSvgImages/SellHomeSvg/SellHomeSvg";
+import ConstructionSvg from "../../../conmponents/AllSvgImages/ConstructionSvg/ConstructionSvg";
+import ApartioSvg from "../../../conmponents/AllSvgImages/ApartioSvg/ApartioSvg";
+import ApartmentsSvg from "../../../conmponents/AllSvgImages/ApartmentsSvg/ApartmentsSvg";
+import AreaSvg from "../../../conmponents/AllSvgImages/AreaSvg/AreaSvg";
 
 const MainFocus = () => {
   return (
@@ -10,12 +16,8 @@ const MainFocus = () => {
           <h2>Our Main Focus</h2>
         </div>
 
-        <div></div>
-
         <div className="main_focus_card_outer_container">
-
           <div className="focus_parcard_container">
-
             <div className="svg_image_outer_container">
               <div className="svg_image_inner_container">
                 <BuyHomeSvg></BuyHomeSvg>
@@ -29,57 +31,86 @@ const MainFocus = () => {
                 match you with a house you will want to call home.
               </p>
               <Link>
-                <span>Find Home -&gt; </span> 
+                <span>Find Home -&gt; </span>
               </Link>
-
             </div>
-
           </div>
 
           <div className="focus_parcard_container">
-
             <div className="svg_image_outer_container">
               <div className="svg_image_inner_container">
-                <BuyHomeSvg></BuyHomeSvg>
+                <RentHomeSvg></RentHomeSvg>
               </div>
             </div>
 
             <div className="focus_section_detais_container">
-              <h3>Buy a Home</h3>
+              <h3>Rent a Home</h3>
               <p>
                 over 1 million+ homes for sale available on the website, we can
                 match you with a house you will want to call home.
               </p>
               <Link>
-                <span>Find Home -&gt; </span> 
+                <span>Find Home -&gt; </span>
               </Link>
-
             </div>
-
           </div>
 
           <div className="focus_parcard_container">
-
             <div className="svg_image_outer_container">
               <div className="svg_image_inner_container">
-                <BuyHomeSvg></BuyHomeSvg>
+                <SellHomeSvg></SellHomeSvg>
               </div>
             </div>
 
             <div className="focus_section_detais_container">
-              <h3>Buy a Home</h3>
+              <h3>Sell a Home</h3>
               <p>
                 over 1 million+ homes for sale available on the website, we can
                 match you with a house you will want to call home.
               </p>
               <Link>
-                <span>Find Home -&gt; </span> 
+                <span>Find Home -&gt; </span>
               </Link>
+            </div>
+          </div>
+        </div>
 
+        <div className="main_focus_builder_info_outer_container">
+          <div className="main_focus_builder_info_inner_container">
+
+            <div className="builder_par_section_container">
+              <div className="builder_Info_icon_cntainer">
+                <ConstructionSvg></ConstructionSvg>
+              </div>
+              <h3>300+</h3>
+              <p>Total Construction</p>
+            </div>
+
+            <div className="builder_par_section_container">
+              <div className="builder_Info_icon_cntainer">
+                <ApartioSvg></ApartioSvg>
+              </div>
+              <h3>140+</h3>
+              <p>Apartio Rooms</p>
+            </div>
+
+            <div className="builder_par_section_container">
+              <div className="builder_Info_icon_cntainer">
+                <ApartmentsSvg></ApartmentsSvg>
+              </div>
+              <h3>250+</h3>
+              <p>Apartments Sold</p>
+            </div>
+
+            <div className="builder_par_section_container">
+              <div className="builder_Info_icon_cntainer">
+                <AreaSvg></AreaSvg>
+              </div>
+              <h3>710+</h3>
+              <p>Total Area Sq</p>
             </div>
 
           </div>
-
         </div>
       </section>
     </>
