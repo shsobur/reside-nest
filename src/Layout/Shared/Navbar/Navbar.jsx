de1defe4304bb9fbd6eda747b1773514efc46278
shsobur <shsoburhossen951@gmail.com>
2024-08-28 22:03:12 +0600

    Navbar responsive design completed

--- src/Layout/Shared/Navbar/Navbar.jsx
+++ src/Layout/Shared/Navbar/Navbar.jsx
@@ -1,8 +1,17 @@
-import { NavLink } from "react-router-dom";
 import "../Navbar/Navbar.css";
+import { NavLink } from "react-router-dom";
 import { FaRegCircleUser } from "react-icons/fa6";
+import { useState } from "react";
+import { IoIosMenu } from "react-icons/io";
+import { RxCross2 } from "react-icons/rx";
 
 const Navbar = () => {
+  const [burgerMenu, setBurgerMenu] = useState(false);
+
+  const handelBurgerMenuIcon = () => {
+    setBurgerMenu(!burgerMenu);
+  }
+
   return (
     <>
       <nav className="main_nav_bar_outer_container">
@@ -11,12 +20,14 @@ const Navbar = () => {
             <h2>ResideNest</h2>
           </div>
 
-          <div className="main_navigater_outer_container">
+          <div id="main_navigater_outer_container">
             <ul>
               <NavLink
                 to="/"
                 className={({ isActive }) =>
-                  isActive ? "text-[#8b5cf4] text-xl font-bold underline" : "text-[#151515] text-xl font-bold" 
+                  isActive
+                    ? "text-[#8b5cf4] text-lg font-bold underline"
+                    : "text-[#151515] text-lg font-bold"
                 }
               >
                 <li>Home</li>
@@ -25,7 +36,9 @@ const Navbar = () => {
               <NavLink
                 to="/updateprofile"
                 className={({ isActive }) =>
-                  isActive ? "text-[#8b5cf4] text-xl font-bold underline" : "text-[#151515] text-xl font-bold" 
+                  isActive
+                    ? "text-[#8b5cf4] text-lg font-bold underline"
+                    : "text-[#151515] text-lg font-bold"
                 }
               >
                 <li>Update Profile</li>
@@ -34,7 +47,9 @@ const Navbar = () => {
               <NavLink
                 to="/contact"
                 className={({ isActive }) =>
-                  isActive ? "text-[#8b5cf4] text-xl font-bold underline" : "text-[#151515] text-xl font-bold" 
+                  isActive
+                    ? "text-[#8b5cf4] text-lg font-bold underline"
+                    : "text-[#151515] text-lg font-bold"
                 }
               >
                 <li>Contact</li>
@@ -43,7 +58,9 @@ const Navbar = () => {
               <NavLink
                 to="/aboutus"
                 className={({ isActive }) =>
-                  isActive ? "text-[#8b5cf4] text-xl font-bold underline" : "text-[#151515] text-xl font-bold" 
+                  isActive
+                    ? "text-[#8b5cf4] text-lg font-bold underline"
+                    : "text-[#151515] text-lg font-bold"
                 }
               >
                 <li>About Us</li>
@@ -57,8 +74,22 @@ const Navbar = () => {
             <div className="nav_user_icon">
               <FaRegCircleUser />
             </div>
-          </div>
 
+            <div
+              onClick={handelBurgerMenuIcon}
+              className="burger_menu_container"
+            >
+              {burgerMenu ? (
+                <div>
+                  <IoIosMenu />
+                </div>
+              ) : (
+                <div>
+                  <RxCross2 />
+                </div>
+              )}
+            </div>
+          </div>
         </div>
       </nav>
     </>
